68d59d0cb3b84f50886d384aebfd57598340f55d
Toshihiro Murata <eb68276@jp.ibm.com>
2015-06-22 16:27:33 +0900

    Update test cases and codes

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,13 @@
 class Main
 
-  def get_unmatch target_array
+  def initialize args
+    @args = args
+  end
+
+  def get_unmatch 
+    target_array = args_to_array()
     unless is_valid_length? target_array
-      raise "Invalid parameter length!"
+      raise "Invalid parameter length: [#{target_array.length}]!"
     end
 
     full_set = (1..10000).to_a
@@ -10,17 +15,25 @@ class Main
     result.first
   end
 
-  def args_to_array args_str
+  private
+
+  def args_to_array
+    if @args[0] and File.exist? @args[0]
+      @args = file_to_array(@args[0])
+    end
+
     begin
-      obj = eval(args_str)
-      raise "Invalid value for args_to_array(): #{args_str}!" unless (obj and obj.instance_of? Array)
+      obj = eval(@args.to_s)
+      raise "Invalid value for args_to_array(): #{@args}!" unless (obj and obj.instance_of? Array)
       return obj.compact
-    rescue SyntaxError => ex
-      raise SyntaxError, "Invalid value for args_to_array(): #{args_str}!"
+    rescue SyntaxError
+      raise SyntaxError, "Invalid value for args_to_array(): #{@args}!"
     end
   end
 
-  private
+  def file_to_array file_path
+    return File.read(file_path)
+  end
 
   def is_valid_length? target_array
     if target_array.length == 9999
@@ -29,4 +42,8 @@ class Main
     false
   end
 
+end
+
+if __FILE__ == $0
+    puts Main.new(ARGV).get_unmatch
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Main do
 
   it "args_to_array returns Array object without nil values" do
-    obj = Main.new.args_to_array( '[1,2, 4 , 5, nil,]' )
+    main = Main.new( '[1,2, 4 , 5, nil,]' )
+    obj = main.send(:args_to_array)
     expect( obj.instance_of? Array ).to eq true
     expect( obj.length ).to eq 4
     expect( obj[0] ).to eq 1
@@ -13,20 +14,23 @@ describe Main do
   it "get exception if args_to_array recieves an invalid value" do
     ['','nil','false'].each do |val|
       expect do
-        obj = Main.new.args_to_array(val)
+        main = Main.new(val)
+        main.send(:args_to_array)
       end.to raise_error( RuntimeError, "Invalid value for args_to_array(): #{val}!" )
     end
     
     ['[','*'].each do |val|
       expect do
-        obj = Main.new.args_to_array( val )
+        main = Main.new(val)
+        main.send(:args_to_array)
       end.to raise_error( SyntaxError, "Invalid value for args_to_array(): #{val}!" )
     end
   end
 
   it "get_unmatch return 10000" do
     target_array = (1..9999).to_a.shuffle
-    expect( Main.new.get_unmatch(target_array) ).to eq 10000
+    main = Main.new(target_array.to_s)
+    expect( main.get_unmatch ).to eq 10000
   end
 
   it "get_unmatch return 1" do
@@ -34,7 +38,8 @@ describe Main do
       (1..10000).to_a.shuffle.each do |i|
         target_array.push(i) unless i == 1
       end
-      expect( Main.new.get_unmatch(target_array) ).to eq 1
+      main = Main.new(target_array.to_s)
+      expect( main.get_unmatch ).to eq 1
   end
 
   it "get_unmatch return an unmatched value" do
@@ -43,22 +48,35 @@ describe Main do
         (1..10000).to_a.shuffle.each do |j|
           target_array.push(j) unless i == j
         end
-        expect( Main.new.get_unmatch(target_array) ).to eq i
+        main = Main.new(target_array.to_s)
+        expect( main.get_unmatch ).to eq i
       end
   end
 
   it "get exception if target_array length is not 9999 but 9998" do
+    target_array = (1..9998).to_a.shuffle
     expect do
-      target_array = (1..9998).to_a.shuffle
-      Main.new.get_unmatch(target_array)
-    end.to raise_error( RuntimeError, "Invalid parameter length!" )
+      main = Main.new(target_array.to_s)
+      main.get_unmatch
+    end.to raise_error( RuntimeError, "Invalid parameter length: [#{target_array.length}]!" )
   end
 
   it "get exception if target_array length is not 9999 but 10001" do
+    target_array = (1..10001).to_a.shuffle
     expect do
-      target_array = (1..10001).to_a.shuffle
-      Main.new.get_unmatch(target_array)
-    end.to raise_error( RuntimeError, "Invalid parameter length!" )
+      main = Main.new(target_array.to_s)
+      main.get_unmatch
+    end.to raise_error( RuntimeError, "Invalid parameter length: [#{target_array.length}]!" )
+  end
+
+  it "file_to_array returns Array.to_s value by using a file text" do
+    file_path = '.tmp_rspec_file_to_array_001'
+    text = (1..9999).to_a.shuffle.to_s
+    File.write(file_path, text)
+    target_array = (1..9999).to_a.shuffle
+    main = Main.new(target_array.to_s)
+    text2 = main.send(:file_to_array, file_path)
+    expect( text ).to eq text2
   end
 
 end
